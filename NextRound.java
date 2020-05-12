e37469f61a3aa87a4e62d46ee538def6c387f6ee
Himanshu Gawari <himanshu25031991@gmail.com>
2020-05-12 10:49:25 +0530

    158A - NextRound

---
+++ NextRound.java
@@ -0,0 +1,22 @@
+import java .util.Scanner;
+
+public class NextRound {
+ public static void main(String[] args){
+   Scanner sc=new Scanner(System.in);
+   int n=sc.nextInt();
+   int k=sc.nextInt();
+   int counter=0;
+   int i=0;
+   int[] array=new int[n];
+   while(n--!=0){
+     array[i++]=sc.nextInt();
+   }
+   int p=array[k-1];
+   for(Integer a:array){
+     if(a>0 & a>=p){
+       counter++;
+     }
+   }
+   System.out.println(counter);
+ }
+}
