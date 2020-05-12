96c93fd2674186773c427220fcd27aa774836181
Himanshu Gawari <himanshu25031991@gmail.com>
2020-05-12 16:32:17 +0530

    231A - Team

--- Team.java
+++ Team.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Team {
-  public static void main(String[] args){
+   public static void main(String[] args){
     Scanner sc=new Scanner(System.in);
     int t=sc.nextInt();
     int counter=0;
