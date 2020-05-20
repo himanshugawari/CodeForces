036b733db95da90548dc54c41d48188ce36409d5
Himanshu Gawari <himanshu25031991@gmail.com>
2020-05-20 20:26:08 +0530

    112A - PetyaAndStrings

---
+++ PetyaAndStrings.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class PetyaAndStrings {
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    String str1=sc.nextLine();
+    String str2=sc.nextLine();
+    char[] arr1=new char[str1.length()];
+    char[] arr2=new char[str2.length()];
+    for(int i=0;i<str1.length();i++){
+      arr1[i]=Character.toLowerCase(str1.charAt(i));
+      arr2[i]=Character.toLowerCase(str2.charAt(i));
+    }
+    int counter=0;
+    for(int i=0;i<str1.length();i++){
+      if(arr1[i]<arr2[i]){
+        System.out.println(-1);
+        break;
+      }else if(arr1[i]>arr2[i]){
+        System.out.println(1);
+        break;
+      }else{
+        counter++;
+      }
+    }
+    if(counter==str1.length()){
+      System.out.println(0);
+    }
+    sc.close();
+  }
+}
