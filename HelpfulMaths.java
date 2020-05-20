c8f88a7f3281082891846a06c6e2befcc87932fd
Himanshu Gawari <himanshu25031991@gmail.com>
2020-05-20 21:22:04 +0530

    339A - Helpful Maths

--- HelpfulMaths.java
+++ HelpfulMaths.java
@@ -1,3 +1,17 @@
+import java.util.*;
 public class HelpfulMaths {
-  
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    String str=sc.nextLine();
+    String[] arr=str.split("\\+");
+    Arrays.sort(arr);
+    for (int i=0;i<arr.length;i++){
+      if(i<arr.length-1){
+        System.out.print(arr[i]+"+");
+      }else{
+        System.out.print(arr[i]);
+      }
+    }
+    sc.close();
+  }
 }
