a718aa5e5ddaa23d604c727b62313ae97e114da6
Himanshu Gawari <himanshu25031991@gmail.com>
2020-05-21 19:45:05 +0530

    266A - Stones on the Table

--- StonesOnTheTable.java
+++ StonesOnTheTable.java
@@ -1,3 +1,17 @@
+import java.util.*; 
+
 public class StonesOnTheTable {
-  
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    String str=sc.next();
+    int count=0;
+    for(int i=0;i<n-1;i++){
+      if(str.charAt(i)==str.charAt(i+1)){
+        count++;
+      }
+    }
+    System.out.println(count);
+    sc.close();
+  }
 }
