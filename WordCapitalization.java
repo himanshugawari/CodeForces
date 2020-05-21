a11fcf8d08851d87266718755edd4b96a560cc4c
Himanshu Gawari <himanshu25031991@gmail.com>
2020-05-21 18:54:25 +0530

    281A - Word Capitalization

--- WordCapitalization.java
+++ WordCapitalization.java
@@ -1,3 +1,12 @@
+import java.util.*;
+
 public class WordCapitalization {
-  
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    String cap=sc.nextLine();
+    StringBuilder str=new StringBuilder(cap);
+    str.setCharAt(0, Character.toUpperCase(cap.charAt(0)));
+    System.out.println(str);
+    sc.close();
+  }
 }
