756578479e73e48fc1b9635a722f5d88f5292ad0
Himanshu Gawari <himanshu25031991@gmail.com>
2020-05-23 22:51:31 +0530

    118A - String Task

--- StringTask.java
+++ StringTask.java
@@ -1,3 +1,22 @@
+import java.util.*;
+
 public class StringTask {
-  
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    String str=sc.next();
+    char[] v=new char[]{'A','a','E','e','O','o','Y','y','U','u','I','i'};
+    boolean flag=false;
+    for(int i=0;i<str.length();i++){
+      flag=false;
+      for(int j=0;j<v.length;j++){
+        if(str.charAt(i)==v[j]){
+          flag=true;
+        }
+      }
+      if(flag!=true){
+        System.out.print("."+Character.toLowerCase(str.charAt(i)));
+      }
+    }
+    sc.close();
+  }
 }
