93210bd63f5acb3c90c66f2590533b796674ea66
Himanshu Gawari <himanshu25031991@gmail.com>
2020-05-23 22:19:28 +0530

    1A - Theatre Square

--- TheatreSquare.java
+++ TheatreSquare.java
@@ -1,3 +1,14 @@
+import java.util.*;
+
 public class TheatreSquare {
-  
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    double n=sc.nextDouble();
+    double m=sc.nextDouble();
+    double a=sc.nextDouble();
+    double t1=Math.ceil(n/a);
+    double t2=Math.ceil(m/a);
+    System.out.println(Math.round(t1*t2));
+    sc.close();
+  }
 }
