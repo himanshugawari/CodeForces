bdd6cba05b272f6359b793e1d0257440242d9886
Himanshu Gawari <himanshu25031991@gmail.com>
2020-05-27 15:38:46 +0530

    791A - Bear and Big Brother

--- BearAndBigBrother.java
+++ BearAndBigBrother.java
@@ -1,3 +1,22 @@
+import java.util.*;
+
 public class BearAndBigBrother {
-  
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    int i=0;
+    while(i<Integer.MAX_VALUE){
+      a*=3;
+      b*=2;
+      if(a>b){
+        i++;
+        break;
+      }else{
+        i++;
+      }
+    }
+    System.out.println(i);
+    sc.close();
+  }
 }
