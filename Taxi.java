ccb158b9e2b5f2146e6b2d927fd5a323eb52a936
Himanshu Gawari <himanshu25031991@gmail.com>
2020-06-02 11:45:29 +0530

    init

--- Taxi.java
+++ Taxi.java
@@ -1,3 +1,11 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Taxi {
-  
+  public static void main(String[] args) throws IOException{
+    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    
+    br.close();
+  }
 }
