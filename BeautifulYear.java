00e8c9912a6d213fb29ed54224b2c0c7228833fd
Himanshu Gawari <himanshu25031991@gmail.com>
2020-06-08 22:34:39 +0530

    271A - Beautiful Year

--- BeautifulYear.java
+++ BeautifulYear.java
@@ -1,5 +1,28 @@
+// 271A - Beautiful Year
+
+import java.util.Scanner;
+
 public class BeautifulYear {
   public static void main(String[] args) {
-    
+    Scanner sc=new Scanner(System.in);
+    int y=sc.nextInt();
+    y++;
+    sc.close();
+    boolean f=false;
+    do{
+    f=false;
+    for(int i=0;i<4;i++){
+      String s=String.valueOf(y);
+      for(int j=i+1;j<4;j++){
+        if(s.charAt(i)==s.charAt(j)){
+          f=true;
+        }
+      }
+    }
+    if(f){
+      y++;
+    }
+  }while(f);
+  System.out.println(y);
   }
 }
