dfd8d0c203e186efff8275f45cb7b7debdfe00c0
Himanshu Gawari <himanshu25031991@gmail.com>
2020-06-10 21:21:46 +0530

    136A - Presents

--- Presents.java
+++ Presents.java
@@ -1,17 +1,21 @@
 // 136A - Presents
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Presents {
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int n = sc.nextInt();
-    int[] t = new int[n + 10];
+  public static void main(String[] args) throws IOException {
+    /*
+     * Scanner sc = new Scanner(System.in); int n = sc.nextInt(); int[] t = new
+     * int[n + 10]; for (int i = 0; i < n; ++i) { t[i] = sc.nextInt(); } sc.close();
+     */
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
     int[] s = new int[n + 10];
-    for (int i = 0; i < n; ++i) {
-      t[i] = sc.nextInt();
-    }
-    sc.close();
+    int[] t = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+    br.close();
     for (int i = 0; i < n; ++i) {
       s[t[i]] = i + 1;
     }
