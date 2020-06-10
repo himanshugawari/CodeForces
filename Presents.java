01f6fb7c70b10754e544fcfcd7a5c0311e20534a
Himanshu Gawari <himanshu25031991@gmail.com>
2020-06-10 21:11:19 +0530

    136A - Presents

--- Presents.java
+++ Presents.java
@@ -1,3 +1,22 @@
-public class Presents {
+// 136A - Presents
 
+import java.util.Scanner;
+
+public class Presents {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int[] t = new int[n + 10];
+    int[] s = new int[n + 10];
+    for (int i = 0; i < n; ++i) {
+      t[i] = sc.nextInt();
+    }
+    sc.close();
+    for (int i = 0; i < n; ++i) {
+      s[t[i]] = i + 1;
+    }
+    for (int i = 1; i <= n; ++i) {
+      System.out.print(s[i] + " ");
+    }
+  }
 }
