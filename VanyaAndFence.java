dbc66d9359f8c9ed6df6d9eabd6744e8aa113287
Himanshu Gawari <himanshu25031991@gmail.com>
2020-06-11 22:02:32 +0530

    677A - Vanya and Fence

--- VanyaAndFence.java
+++ VanyaAndFence.java
@@ -1,3 +1,25 @@
-public class VanyaAndFence {
+// 677A - Vanya and Fence
 
+import java.util.Scanner;
+
+public class VanyaAndFence {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int h = sc.nextInt();
+    int[] p = new int[n];
+    for (int i = 0; i < n; i++) {
+      p[i] = sc.nextInt();
+    }
+    sc.close();
+    int w = 0;
+    for (int i = 0; i < n; i++) {
+      if (p[i] > h) {
+        w += 2;
+      } else {
+        w++;
+      }
+    }
+    System.out.println(w);
+  }
 }
